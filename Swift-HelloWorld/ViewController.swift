828f93c95e04d45933e0101e0106578412c83e48
Rian Rainey <rian@rianrainey.com>
2014-06-04 20:02:10 -0500

    Initial Commit

---
+++ Swift-HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-HelloWorld
+//
+//  Created by Rian Rainey on 6/4/14.
+//  Copyright (c) 2014 Rian Rainey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
